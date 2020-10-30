d12617f98bcc3be89da3d2a65c1a70e411806bbe
Andrew Player <andrewplayer3@gmail.com>
2020-10-29 20:16:39 -0500

    usage comment

--- steg.cpp
+++ steg.cpp
@@ -7,6 +7,21 @@
 #include <stdio.h>
 #include <fstream>
 
+// Usage:
+//
+// For best results, the message should be hidden in a png file.
+// The message is simply concatenated to the end of the file.
+//
+// Use Case 1:
+// ./steg.exe filename "message"
+//
+// Use Case 2:
+// ./steg.exe filename
+// Please Enter a Message: "message"
+//
+//Use Case 3:
+// cat message.txt | ./steg.exe filename
+
 int main(int argc, char* argv[]) {
     if(argv[1] == nullptr) {
         std::cout << "ERROR: Please enter a valid filename" << std::endl;
