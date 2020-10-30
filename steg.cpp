6a3bafd06d1c0efaac3b9dc4ea86ca230c1fda0e
Andrew Player <andrewplayer3@gmail.com>
2020-10-29 20:02:46 -0500

    added return 0

--- steg.cpp
+++ steg.cpp
@@ -1,3 +1,7 @@
+// Very Basic PNG Steganography
+// By Andrew Player and Robert Lawton
+// Fall 2020
+
 #include <iostream>
 #include <vector>
 #include <stdio.h>
@@ -8,10 +12,15 @@ int main(int argc, char* argv[]) {
         std::cout << "ERROR: Please enter a valid filename" << std::endl;
         return 1;
     }
-    
+
+    if(argv[2] == nullptr) {
+        std::cout << "ERROR: Please enter a message after the filename" << std::endl;
+        return 1;
+    }
+
     std::ofstream out;
     out.open(argv[1], std::ios::app);
     out << argv[2];
-    
-}
 
+    return 0;
+}
